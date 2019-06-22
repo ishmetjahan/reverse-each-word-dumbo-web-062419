cd0028179de17eeab8e3ab3e06730774787999d3
Ishmet <ishmetjahan@yahoo.com>
2019-06-22 11:39:57 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+## Reverse with .each
+def reverse_each_word (str)
+newArr= []
+arr= str.split(" ")
+arr.each do |ele|
+  newArr<< ele.reverse
+end
+return newArr.join(" ")
+return newArr
+end
+
+## Reverse with .collect 
+def reverse_each_word (str)
+  str_split= str.split
+  str_split.colect do |ele|
+  #str.collect do |ele|
+    ele.reverse
+  end
+  str_split
+end 
